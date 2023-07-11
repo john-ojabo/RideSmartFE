e4b3d0e02b565f4f486bf4430f5db4605a8fc4ba
Vicolas11 <incrediblechamp1@gmail.com>
2023-07-11 11:50:20 +0100

    refactor:Remove .env variables that are required

--- src/configs/constant.config.ts
+++ src/configs/constant.config.ts
@@ -1,5 +1,4 @@
 import { IConstant } from "../interfaces/constant.interface";
-import { Secret } from "jsonwebtoken";
 import { config } from "dotenv";
 
 config();
@@ -8,11 +7,6 @@ export const constant: IConstant = {
   expiresIn: "2h",
   refreshIn: "3d",
   secretKey: process.env.SECRET_KEY as string,
-  adminAvatar: process.env.ADMIN_AVATAR as string,
-  accessToken: process.env.ACCESS_TOKEN as Secret,  
-  refreshToken: process.env.REFRESH_TOKEN as Secret,
-  cipherSecretKey: process.env.CIPHER_SECRET_KEY as string,
-  cryptoSecretKey: process.env.CRYPTO_SECRET_KEY as string,
   FLWPaymentEndpoint: process.env.FLWPAYMENT_ENDPOINT as string,
   FLWSECKTest: process.env.FLWSECK_TEST as string,
   FLWPUBKTest: process.env.FLWPUBK_TEST as string,
--- src/interfaces/constant.interface.ts
+++ src/interfaces/constant.interface.ts
@@ -4,11 +4,6 @@ export interface IConstant {
   secretKey: string;
   expiresIn: string;
   refreshIn: string;
-  adminAvatar: string;
-  accessToken: Secret;
-  refreshToken: Secret;
-  cryptoSecretKey: string;
-  cipherSecretKey: string;
   FLWPaymentEndpoint: string;
   FLWSECKTest: string;
   FLWPUBKTest: string;
