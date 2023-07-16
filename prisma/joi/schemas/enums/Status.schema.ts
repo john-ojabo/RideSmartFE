50974504af9070ea1efffc1ac5a74514b407c2dc
Vicolas11 <incrediblechamp1@gmail.com>
2023-07-16 22:27:51 +0100

    feat: remove Expired and add Refunded to Ticket status

--- prisma/joi/schemas/enums/Status.schema.ts
+++ prisma/joi/schemas/enums/Status.schema.ts
@@ -1,3 +1,3 @@
 import Joi from 'joi';
 
-export const StatusSchema = Joi.string().valid(...["Cancelled","Active","Expired","Paid"])
+export const StatusSchema = Joi.string().valid(...["Cancelled","Active","Refunded","Paid","Used"])
